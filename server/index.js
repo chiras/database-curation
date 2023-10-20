fc7dfbd566d094ef5eaa8294cb265d85d1822410
Markus Ankenbrand <markus@ankenbrand.me>
2023-10-20 15:42:45 +0200

    Add basic api route

--- server/index.js
+++ server/index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mysql = require('mysql')
+const connection = mysql.createConnection({host: "localhost", user: "example-user", password: "my_cool_secret", database: "dbcuration"})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/api', (req, res) => {
+  connection.query(
+    'SELECT * FROM its2_accessions LIMIT 10',
+    (err, rows, fields) => {
+      res.json(rows)
+    }
+  );
+})
+
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
