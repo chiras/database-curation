c521879f8ce9169e1ed950fa28f601deeeefe048
Markus Ankenbrand <markus@ankenbrand.me>
2023-10-26 15:44:53 +0200

    Add column ordering

--- server/index.js
+++ server/index.js
@@ -8,16 +8,25 @@ const connection = mysql.createConnection({host: "localhost", user: "example-use
 let records_total = -1
 connection.query('SELECT COUNT(*) AS total FROM its2_accessions', (err, rows, fields) => {records_total = rows[0].total})
 
+const columns=["Accession", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species", "Status"]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/api', (req, res) => {
+  //console.log(req.query)
   let start = parseInt(req.query["start"])
   let len = parseInt(req.query["length"])
+  let order_cols = req.query["order"].map(
+	  x => [req.query["columns"][parseInt(x["column"])]["data"], x["dir"].toUpperCase()]
+      ).filter(
+	  x => columns.includes(x[0])
+      ).map(
+	  x => x.join(" ").replace("Order", "`Order`")
+      ).join(", ")
   connection.query(
-    `SELECT * FROM its2_accessions LIMIT ${len} OFFSET ${start}`,
+    `SELECT * FROM its2_accessions ORDER BY ${order_cols} LIMIT ${len} OFFSET ${start}`,
     (err, rows, fields) => {
 	    //console.log(req)
       res.json({
